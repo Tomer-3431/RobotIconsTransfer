dcf2e4cdcea2214fb0dc48212a76558dc758fae0
Hatulim <tomer10.katz@gmail.com>
2024-12-30 14:09:21 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,32 @@
+import shutil
+
+userName = "user"
+wpilibVersion = 2025
+materialIconVersion = "5.16.0"
+meterialIconsPath = "/extensions/pkief.material-icon-theme-" + materialIconVersion + "/icons"
+fullIconPath = ""
+
+icons = ["folder-chassis", "folder-chassis-open"]
+
+while True:
+  print("what are you using vscode or wpilib vscode:\n1. vscode\n2. wpilib")
+  response = input().lower()
+
+  if response == "1" or response == "vscode":
+    print("what is your user name in windows:")
+    userName = input()
+    fullIconPath = "C:/Users/" + userName + "/.vscode" + meterialIconsPath
+    break
+
+  elif response == "2" or response == "wpilib":
+    print("what is your wpilib year version:")
+    wpilibVersion = input()
+    fullIconPath = "C:/Users/Public/wpilib/" + wpilibVersion + "/vscode/data" + meterialIconsPath
+    break
+  else:
+    print("illegel choice please try again")
+
+for i in icons:
+  shutil.copy("./icons/" + i + ".svg", fullIconPath)
+
+print("Fully moved all the icons into meterial icons")
